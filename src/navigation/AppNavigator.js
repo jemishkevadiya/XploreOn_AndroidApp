38ffdf5e5b4db9aeaf8a38dcfb838436de55c33d
jemish kevadiya <jemishkevadiya007@gmail.com>
2024-11-23 00:08:58 -0500

    Added services screen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,18 +3,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from '../screens/SplashScreen';
 import HomeScreen from '../screens/HomeScreen';
+import ServicesScreen from '../screens/ServicesScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen">
-        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen}  options={{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="SplashScreen"
+             screenOptions={{
+                        headerBackImage: () => (
+                            <Ionicons name="arrow-back" size={25} color="#fff" style={{ marginLeft: 10 }} />
+                        ),
+                        headerBackTitleVisible: false, // Hides the "Back" text
+                        headerStyle: {
+                            backgroundColor: '#ff6f00', // Header background color
+                        },
+                        headerTintColor: '#fff', // Color for icons and text
+                    }}>
+                <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="ServicesScreen" component={ServicesScreen} options={{ headerShown: false }}/>
+
+                
+                {/* Individual Service Screens */}
+                
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default AppNavigator;
