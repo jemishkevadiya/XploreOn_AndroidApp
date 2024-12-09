65a2b39e9af81b87a75e3d04588541340188123b
jemish kevadiya <jemishkevadiya007@gmail.com>
2024-12-08 23:58:35 -0500

    Changed new API key to avoid free limits

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
     API_KEY: '4d849cc576mshcb03501f5856895p15f1d5jsn618ddc0e3766',
     API_HOST: 'booking-com15.p.rapidapi.com',
-    API_KEY_RES: '29467b397cmsh314ed40ad11077ep1f6d99jsn468a0fb314f1',
+    API_KEY_RES: '31bb0d4fdfmshf015ba1f6e28e2bp1aa683jsn072b839b93df',
     API_HOST_RES: 'tripadvisor16.p.rapidapi.com'
   };
   
